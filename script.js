3d2e59a95c1eecfa55f8ad83442b6a0d50a771bd
SungSubKim <37684243+SungSubKim@users.noreply.github.com>
2018-07-19 10:00:47 +0900

    180719
    ..

---
+++ script.js
@@ -0,0 +1,19 @@
+function goToLoading(num) {
+  //페이
+  if(num===1) {
+
+  }
+  //이체
+  else if(num===2) {
+
+  }
+  //보험
+  else if(num===3) {
+
+  }
+  //대출
+  else {
+
+  }
+  location.href="loading.html";
+}
